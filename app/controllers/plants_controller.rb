bc0e3e90c374427090f7a9f506a03e3327ee36bb
edgar ludena <edgar.ludena90@gmail.com>
2023-01-17 10:13:04 -0500

    commit tree 82d97f4e068bbe4165b51fdef878f386bd4eb286

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,20 @@
+class PlantsController < ApplicationController
+
+  def index 
+    plants = Plant.all 
+    render json: plants
+  end
+  def show
+    plants = Plant.find_by(id: params[:id])
+    if plants
+      render json: plants
+    else
+      render json: { error: "Plant not found" }, status: :not_found
+    end
+  end
+  def create
+    plants = Plant.create(name: params[:name], image: params[:image],price: params[:price])
+    render json: plants, status: :created
+ end
+end
+
